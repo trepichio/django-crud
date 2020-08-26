24d6344edda562e90b7cfa5a86f9ab8e2c104267
João Trepichio <trepichio@gmail.com>
2020-08-26 17:46:36 -0300

    add Date Input in customer create form

--- customer/forms.py
+++ customer/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from .models import Customer
 
+class DateInput(forms.DateInput):
+  input_type="date"
+
 class CustomerForm(forms.ModelForm):
     first_name = forms.CharField(label="Nome")
     last_name = forms.CharField(label="Sobrenome")
     email = forms.EmailField(label="E-mail")
-    birth_date = forms.DateField(label="Data de Nascimento")
+    birth_date = forms.DateField(label="Data de Nascimento", widget=DateInput())
     area_code = forms.CharField(label="DDD")
     phone_number = forms.CharField(label="Telefone")
     country = forms.CharField(label="País")
