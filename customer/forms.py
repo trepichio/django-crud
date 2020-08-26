cdb2a3a90892e06465f9098fe5c0e763d58f3995
João Trepichio <trepichio@gmail.com>
2020-08-26 18:52:03 -0300

    add form validations and custom error messages

--- customer/forms.py
+++ customer/forms.py
@@ -5,12 +5,26 @@ class DateInput(forms.DateInput):
   input_type="date"
 
 class CustomerForm(forms.ModelForm):
-    first_name = forms.CharField(label="Nome")
-    last_name = forms.CharField(label="Sobrenome")
+    first_name = forms.CharField(
+      label="Nome",
+      error_messages={"max_length": "Nome não pode ter mais que 30 caracteres"}
+    )
+    last_name = forms.CharField(
+      label="Sobrenome",
+      error_messages={"max_length": "Sobrenome não pode ter mais que 30 caracteres"}
+    )
     email = forms.EmailField(label="E-mail")
     birth_date = forms.DateField(label="Data de Nascimento", widget=DateInput())
-    area_code = forms.CharField(label="DDD")
-    phone_number = forms.CharField(label="Telefone")
+    area_code = forms.RegexField(
+      label="DDD",
+      regex=r"^(\+55)?[0-9]{2}$",
+      error_messages={"invalid": "Número de DDD inválido."}
+    )
+    phone_number = forms.RegexField(
+      label="Telefone",
+      regex=r"^\+?1?[0-9]{9,15}$",
+      error_messages={"invalid": "Número de telefone inválido."}
+    )
     country = forms.CharField(label="País")
     state = forms.CharField(label="Estado")
     city = forms.CharField(label="Cidade")
