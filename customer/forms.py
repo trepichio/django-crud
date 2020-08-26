104a692b9656c9c9b0b27ba2d991f294068698c6
João Trepichio <trepichio@gmail.com>
2020-08-26 17:40:32 -0300

    add customer create form

---
+++ customer/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Customer
+
+class CustomerForm(forms.ModelForm):
+    first_name = forms.CharField(label="Nome")
+    last_name = forms.CharField(label="Sobrenome")
+    email = forms.EmailField(label="E-mail")
+    birth_date = forms.DateField(label="Data de Nascimento")
+    area_code = forms.CharField(label="DDD")
+    phone_number = forms.CharField(label="Telefone")
+    country = forms.CharField(label="País")
+    state = forms.CharField(label="Estado")
+    city = forms.CharField(label="Cidade")
+
+    class Meta:
+        model = Customer
+        fields = (
+          "first_name",
+          "last_name",
+          "email",
+          "birth_date",
+          "area_code",
+          "phone_number",
+          "country",
+          "state",
+          "city"
+        )
--- customer/views.py
+++ customer/views.py
@@ -1,9 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from .models import Customer
+from .forms import CustomerForm
 
 
 class CustomerListView(ListView):
     template_name = "customer/customer_list.html"
     model = Customer
     queryset = Customer.objects.all()
+
+class CustomerCreateView(CreateView):
+    template_name = "customer/customer.html"
+    form_class = CustomerForm
+
+    def form_valid(self, form):
+      return super().form_valid(form)
